df207ebdbe4a474e8cb4f171c9086ba5501d7193
anubhav13@gmail.com <Evolution2019!>
2019-09-18 20:55:51 -0700

    First commit. Testing

--- Saturn/main.py
+++ Saturn/main.py
@@ -3,22 +3,22 @@ import logging
 
 def main():
     logger = logging.getLogger()
-    logger.setLevel(logging.DEBUG)
-    # logger.setLevel(logging.ERROR)
+    # logger.setLevel(logging.DEBUG)
+    logger.setLevel(logging.ERROR)
 
     xlsname = "TestModel_v2.xlsx"
     # xlsname = "../Test Models/TestModel_v1.xlsx"
     # xlsname = "../Test Models/LP_LatAm Model vF.xlsx"
 
     excel = Loader(xlsname)
-    set_addr = 'Sheet3!F4'
-    addr = 'Sheet3!F5'
+    set_addr = 'Sheet3!D3'
+    addr = 'Sheet3!E5'
     # c = excel.getCell(addr)
     # val = excel.evaluate(c)
     # print(val)
 
     # addr = 'Returns!D5'
-    excel.setvalue(25,set_addr)
+    excel.setvalue(20,set_addr)
     print(excel.getvalue(addr))
 
 
